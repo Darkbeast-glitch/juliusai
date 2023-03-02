f66b6c0a2d4f1295b4f79c3922ad786212c845b1
Julius <bbjuliu4@gmail.com>
2023-03-02 15:54:33 +0100

    fixed api key again

--- Juliusai/chatgpt_app.py
+++ Juliusai/chatgpt_app.py
@@ -14,7 +14,7 @@ Overall, this web application is a powerful tool that leverages cutting-edge nat
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
 # follow step 4 to get a secret_key
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def main():
