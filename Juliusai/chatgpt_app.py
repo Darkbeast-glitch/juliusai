09650d8aef01cc74ed3cefaf92666bacf4337d35
Julius <bbjuliu4@gmail.com>
2023-03-02 16:40:00 +0100

    added python-dontenv

--- Juliusai/chatgpt_app.py
+++ Juliusai/chatgpt_app.py
@@ -1,6 +1,12 @@
 import openai
 import streamlit as st
-import config
+from dotenv import load_dotenv
+import os
+
+
+def configure():
+    load_dotenv()
+
 
 st.title("JuliusAI + ChatGPT")
 
@@ -14,7 +20,7 @@ Overall, this web application is a powerful tool that leverages cutting-edge nat
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
 # follow step 4 to get a secret_key
-openai.api_key = config.api_key
+openai.api_key = os.getenv('api_key')
 
 
 def main():
