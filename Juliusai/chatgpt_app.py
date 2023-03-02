665cb0aac39c65ede8c91501e48ce7852faea65e
Julius <bbjuliu4@gmail.com>
2023-03-02 16:19:09 +0100

    updated

--- Juliusai/chatgpt_app.py
+++ Juliusai/chatgpt_app.py
@@ -1,6 +1,5 @@
 import openai
 import streamlit as st
-import config
 
 st.title("JuliusAI + ChatGPT")
 
@@ -14,7 +13,7 @@ Overall, this web application is a powerful tool that leverages cutting-edge nat
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
 # follow step 4 to get a secret_key
-openai.api_key = config.api_key
+openai.api_key = ""
 
 
 def main():
